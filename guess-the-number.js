c5563110c4cd6a9eb297e4b66d054be5cfb36750
loxtonsa01 <loxtonsa01@craighead.school.nz>
2021-09-02 18:11:43 +1200

    Number Generator
    Added a random number generator, and a alert after to show the random number picked.

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,6 +1,6 @@
 // Your code goes in this file.
 
-//make and array
+//An array to hold the users inputs
 let playerInfo = [
 
 ]
@@ -10,4 +10,9 @@ let playerName = prompt('Create a username.')
 //putting the input into the array
 playerInfo.push(playerName)
 
-alert(playerInfo)
+alert('welcome to the game ' + playerInfo)
+
+//Getting a random number
+const randomNum = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+alert(randomNum)
+
