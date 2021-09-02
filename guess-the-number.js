b9a6d1ab7f4e92c96a65ac5f0012fdab03040361
loxtonsa01 <loxtonsa01@craighead.school.nz>
2021-09-02 17:45:08 +1200

    User input
    The user can now input their username for later use in the program.

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,2 +1,13 @@
 // Your code goes in this file.
 
+//make and array
+let playerInfo = [
+
+]
+
+//taking the users input
+let playerName = prompt('Create a username.')
+//putting the input into the array
+playerInfo.push(playerName)
+
+alert(playerInfo)
