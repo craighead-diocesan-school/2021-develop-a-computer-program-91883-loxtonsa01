beeb2757ea32b40caa30b79a520be0afe992120f
loxtonsa01 <loxtonsa01@craighead.school.nz>
2021-09-20 19:15:30 +1200

    Added functions
    Added functions to clean up the code and to separately use the functions for a cleaner look.

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,48 +1,59 @@
-//taking the users input
-let playerName = prompt('Create a username.')
+function play() {
+    getName()
+    playGame()
+}
 
-//welcomes the user to the game using their username
-alert('welcome to the game ' + playerName)
+function getName() {
+    //taking the users input
+    let playerName = prompt('Create a username.')
 
-let rerun = 'yes'
+    //welcomes the user to the game using their username
+    alert('welcome to the game ' + playerName)
+}
 
 
-while (rerun == 'yes') {
-    //Getting a random number
-    const randomNum = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
-    alert(randomNum) //////testing//////
+function playGame() {
+    let rerun = 'yes'
 
-    //a counter so the program will stop when the user has guessed 3 times
-    let counter = 0
-    const maxTries = 3
+    while (rerun == 'yes') {
+        //Getting a random number
+        const randomNum = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+        alert(randomNum) //////testing//////
 
-    //while the number of the guesses the user inputs does not equal 3 the loop will continue
-    while (counter <= maxTries) {
+        //a counter so the program will stop when the user has guessed 3 times
+        let counter = 0
+        const maxTries = 3
 
-        //user guesses the number
-        let guess = prompt('pick a number between 1 and 20 inclusive.')
-        //if the users input is a number it will continue into the next if statement
-        if (!isNaN(guess)) {
-            //If the users inputs a number it and will dictate if it is correct or not
-            if (guess == randomNum) {
-                //the number the user guessed is correct
-                alert('CORRECT')
+        //while the number of the guesses the user inputs does not equal 3 the loop will continue
+        while (counter <= maxTries) {
 
-                //adding maxtries to the counter, inturn finishing the game
-                counter += maxTries + 1
+            //user guesses the number
+            let guess = prompt('pick a number between 1 and 20 inclusive.')
+            //if the users input is a number it will continue into the next if statement
+            if (!isNaN(guess)) {
+                //If the users inputs a number it and will dictate if it is correct or not
+                if (guess == randomNum) {
+                    //the number the user guessed is correct
+                    alert('CORRECT')
 
+                    //adding maxtries to the counter, inturn finishing the game
+                    counter += maxTries + 1
+
+                } else {
+                    //the counter goes up by one at every wrong answer
+                    counter += counter + 1
+                }
+
+                //Else if the guess the user input not a number
             } else {
-                //the counter goes up by one at every wrong answer
-                counter += counter + 1
+                alert('Error: Bad input.')
             }
 
-            //Else if the guess the user input not a number
-        } else {
-            alert('Error: Bad input.')
+            //asking the user if they want to play again
+            rerun = prompt('Do you want to play again?')
         }
 
-        //asking the user if they want to play again
-        rerun = prompt('Do you want to play again ' + playerName + '?')
     }
-
 }
+
+
